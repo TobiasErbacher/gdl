43008366f1124153e51dc81fc7b91d55574d8c4e
Tobias Erbacher <tobias@My-MacBook-Pro.local>
2025-04-10 17:50:07 +0200

    Started to reconcile the CoGNN class files into the main.ipynb, and added the MAC .DS_Store to gitignore.

--- Tobias/CoGNN/helper/class_encoder_mol.py
+++ Tobias/CoGNN/helper/class_encoder_mol.py
@@ -1,9 +1,6 @@
 import torch
 from ogb.utils.features import get_atom_feature_dims, get_bond_feature_dims
 
-full_atom_feature_dims = get_atom_feature_dims()
-full_bond_feature_dims = get_bond_feature_dims()
-
 class AtomEncoder(torch.nn.Module):
 
     def __init__(self, emb_dim):
@@ -11,7 +8,7 @@ class AtomEncoder(torch.nn.Module):
 
         self.atom_embedding_list = torch.nn.ModuleList()
 
-        for i, dim in enumerate(full_atom_feature_dims):
+        for _, dim in enumerate(get_atom_feature_dims()):
             emb = torch.nn.Embedding(dim, emb_dim)
             torch.nn.init.xavier_uniform_(emb.weight.data)
             self.atom_embedding_list.append(emb)
@@ -30,7 +27,7 @@ class BondEncoder(torch.nn.Module):
 
         self.bond_embedding_list = torch.nn.ModuleList()
 
-        for i, dim in enumerate(full_bond_feature_dims):
+        for _, dim in enumerate(get_bond_feature_dims()):
             emb = torch.nn.Embedding(dim, emb_dim)
             torch.nn.init.xavier_uniform_(emb.weight.data)
             self.bond_embedding_list.append(emb)
